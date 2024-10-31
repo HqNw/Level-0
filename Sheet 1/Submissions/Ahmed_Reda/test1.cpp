bcb85c8e774947c9713dbfb40b86206a4ca90068
HqNw <redalabeb166@gmail.com>
2024-11-01 00:03:06 +0200

    added random runtime to test1

--- Sheet 1/Submissions/Ahmed_Reda/test1.cpp
+++ Sheet 1/Submissions/Ahmed_Reda/test1.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+
+void testAlgorithm() {
+  srand(time(0));
+  int randomTime = rand() % 5 + 1; // Random time between 1 and 5 seconds
+  std::cout << "Algorithm will take " << randomTime << " seconds." << std::endl;
+  sleep(randomTime);
+  std::cout << "Algorithm completed." << std::endl;
+}
+
 int main() {
-	int i = 2;
+  testAlgorithm();
 }
